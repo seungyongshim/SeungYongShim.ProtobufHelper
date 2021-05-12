01a2a736a581fa8dccba2866f1741c9509048ed9
심승용 <seungyongshim@nexon.co.kr>
2021-05-12 17:27:23 +0900

    버그 수정

--- src/SeungYongShim.ProtobufHelper/ProtoKnownTypes.cs
+++ src/SeungYongShim.ProtobufHelper/ProtoKnownTypes.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Text.RegularExpressions;
 using Google.Protobuf;
 using Google.Protobuf.Reflection;
 using static SeungYongShim.ProtobufHelper.FuntionalHelper;
@@ -9,31 +10,38 @@ using Any = Google.Protobuf.WellKnownTypes.Any;
 
 namespace SeungYongShim.ProtobufHelper
 {
-    public class KnownTypes
+    public class ProtoKnownTypes
     {
-        public KnownTypes()
+        public ProtoKnownTypes()
             : this(from assembly in AppDomain.CurrentDomain.GetAssemblies()
+                   let name = assembly.GetName().Name
+                   where !Regex.IsMatch(name, @"^Google.*")
+                   where !Regex.IsMatch(name, @"^Grpc.*")
+                   where !Regex.IsMatch(name, @"^xunit.*")
+                   where !Regex.IsMatch(name, @"^Microsoft.*")
+                   where !Regex.IsMatch(name, @"^System.*")
+                   where !Regex.IsMatch(name, @"^OpenTelemetry.*")
                    select assembly)
         {
 
         }
 
 
-        public KnownTypes(IEnumerable<Type> types)
+        public ProtoKnownTypes(IEnumerable<Type> types)
             : this(from t in types
                    select Assembly.GetAssembly(t))
         {
             
         }
 
-        public KnownTypes(IEnumerable<Assembly> assemblies)
+        public ProtoKnownTypes(IEnumerable<Assembly> assemblies)
         {
             var messageTypes = (
                 from assembly in assemblies
                 from type in assembly.GetTypes()
                 where typeof(IMessage).IsAssignableFrom(type)
                 where type.IsInterface is false
-                select type).ToList();
+                select type).Distinct().ToList();
 
             var descriptorAll =
                 from type in messageTypes
@@ -58,8 +66,8 @@ namespace SeungYongShim.ProtobufHelper
 
         public IMessage Unpack(Any any) => AnyUnpackersDic[any.TypeUrl](any);
 
-        public Dictionary<string, Func<Any, IMessage>> AnyUnpackersDic { get; }
-        public TypeRegistry Registry { get; private set; }
+        internal Dictionary<string, Func<Any, IMessage>> AnyUnpackersDic { get; }
+        internal TypeRegistry Registry { get;  }
         public JsonParser JsonParser { get; }
         public JsonFormatter JsonFormatter { get; }
     }
